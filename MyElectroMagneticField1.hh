160599e4b846bd23ba3996f7899e2dbae8ef9164
locokepler <66323767+locokepler@users.noreply.github.com>
2023-01-26 16:48:18 -0500

    Add files via upload
    The initial files needed as examples

---
+++ MyElectroMagneticField1.hh
@@ -0,0 +1,46 @@
+//
+// ********************************************************************
+// *                                                                  *
+// * Copyright 2022 The TOPAS Collaboration                           *
+// *                                                                  *
+// * Permission is hereby granted, free of charge, to any person      *
+// * obtaining a copy of this software and associated documentation   *
+// * files (the "Software"), to deal in the Software without          *
+// * restriction, including without limitation the rights to use,     *
+// * copy, modify, merge, publish, distribute, sublicense, and/or     *
+// * sell copies of the Software, and to permit persons to whom the   *
+// * Software is furnished to do so, subject to the following         *
+// * conditions:                                                      *
+// *                                                                  *
+// * The above copyright notice and this permission notice shall be   *
+// * included in all copies or substantial portions of the Software.  *
+// *                                                                  *
+// * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,  *
+// * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES  *
+// * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND         *
+// * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT      *
+// * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,     *
+// * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING     *
+// * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR    *
+// * OTHER DEALINGS IN THE SOFTWARE.                                  *
+// *                                                                  *
+// ********************************************************************
+//
+
+#ifndef MyElectroMagneticField1_hh
+#define MyElectroMagneticField1_hh
+
+#include "TsVElectroMagneticField.hh"
+
+class MyElectroMagneticField1 : public TsVElectroMagneticField
+{
+public:
+	MyElectroMagneticField1(TsParameterManager* pM, TsGeometryManager* gM,
+						  TsVGeometryComponent* component);
+	~MyElectroMagneticField1();
+	
+	void GetFieldValue(const G4double[4], G4double *fieldBandE) const;
+	void ResolveParameters();
+};
+
+#endif
